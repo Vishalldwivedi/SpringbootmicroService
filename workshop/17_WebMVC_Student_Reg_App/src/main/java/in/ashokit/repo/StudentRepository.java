ff701d358e71fc2a0e604fab640163605c95df0a
Vishalldwivedi <vdwivedi0109@gmail.com>
2024-12-18 09:45:21 +0530

    Removed specified folders from tracking

--- workshop/17_WebMVC_Student_Reg_App/src/main/java/in/ashokit/repo/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package in.ashokit.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import in.ashokit.entity.StudentEntity;
-
-public interface StudentRepository extends JpaRepository<StudentEntity, Integer>{
-
-}
